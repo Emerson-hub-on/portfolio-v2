5071141b9eb1144d549a0c2605d048dc6c4d3fc5
Emerson Sousa <sousaemerson.bol@gmail.com>
2026-05-06 12:45:25 -0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  vite: {
-    server: {
-      allowedHosts: ['prudishly-dynamite-festive.ngrok-free.dev']
-    }
-  }
 })
